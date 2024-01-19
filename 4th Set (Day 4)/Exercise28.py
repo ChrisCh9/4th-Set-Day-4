e60adc38fe56b27215004779bf7dfbcdde0053a1
ChrisCh9 <151587068+ChrisCh9@users.noreply.github.com>
2024-01-19 23:38:28 +0200

    Semi-Complete
    Semi-Complete

---
+++ 4th Set (Day 4)/Exercise28.py
@@ -0,0 +1,7 @@
+#Update program 027 so that it will display the answer to two decimal places.
+
+num = float(input("Please enter a number with lots of decimals: "))
+
+print(num * 2)
+
+print(round(num),2)
---
+++ 4th Set (Day 4)/Exercise29.py
@@ -0,0 +1,7 @@
+#Ask the user to enter an integer that is over 500. Work out the square root of that number and display it to two decimal places. 
+
+num = int(input("Please enter a number over 500: "))
+
+square_root = num ** (1/2)
+
+print(square_root)
